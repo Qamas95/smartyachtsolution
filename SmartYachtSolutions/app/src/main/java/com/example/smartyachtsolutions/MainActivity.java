3b18ee6fb81e813c515a102781c0d5e3619f401c
Qamas95 <kamil.p.kubiak@gmail.com>
2019-08-22 18:25:02 +0200

    Add listen button Add listen button and code server class

--- SmartYachtSolutions/app/src/main/java/com/example/smartyachtsolutions/MainActivity.java
+++ SmartYachtSolutions/app/src/main/java/com/example/smartyachtsolutions/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.smartyachtsolutions;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,16 +14,21 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 0;
     private static final int REQUEST_DISCOVER_BT = 1;
+    private static final String APP_NAME = "SYS_App";
+    private static final UUID MY_UUID = UUID.fromString("9e67606b-faff-498c-96b1-8a7d3b28b941");
+
 
     TextView mStatusBlueTv, mPairedTv;
     ImageView mBlueIv;
-    Button mOnBtn, mOffBtn, mDiscoverBtn, mPairedBtn;
+    Button mOnBtn, mOffBtn, mDiscoverBtn, mPairedBtn, listenBtn;
 
     BluetoothAdapter mBlueAdapter;
 
@@ -38,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
         mOffBtn       = findViewById(R.id.offBtn);
         mDiscoverBtn  = findViewById(R.id.discoverableBtn);
         mPairedBtn    = findViewById(R.id.pairedBtn);
+        listenBtn = findViewById(R.id.listen);
+
+
+
 
         //adapter
         mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -117,7 +127,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        listenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ServerClass serverClass = new ServerClass();
+
+                serverClass.start();
+            }
+        });
+
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -141,5 +161,44 @@ public class MainActivity extends AppCompatActivity {
     private void showToast(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
+
+
+
+
+
+
+    private class ServerClass extends Thread {
+
+        private BluetoothServerSocket serverSocket;
+        public ServerClass() {
+            try {
+                serverSocket = mBlueAdapter.listenUsingInsecureRfcommWithServiceRecord(APP_NAME,MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+
+            BluetoothSocket socket = null;
+
+            while (socket==null) {
+                try {
+                    socket=serverSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if(socket!=null)
+                {
+                    break;
+                }
+
+            }
+        }
+
+    }
+
+
 
 }
