363464882bb250e16c5329efd555ca0c19e2da20
Qamas95 <kamil.p.kubiak@gmail.com>
2019-09-01 03:36:03 +0200

    Create way to communicate with raspberry //fck yea xdd

--- SmartYachtSolutions/app/src/main/java/com/example/smartyachtsolutions/Connect.java
+++ SmartYachtSolutions/app/src/main/java/com/example/smartyachtsolutions/Connect.java
@@ -2,7 +2,9 @@ package com.example.smartyachtsolutions;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,8 +13,15 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.UUID;
 
 
 public class Connect extends AppCompatActivity {
@@ -53,7 +62,14 @@ public class Connect extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //need to add here function that will run rfcomm transfer and try to connect to raspberry
 
-                Toast.makeText(getApplicationContext(), "You clicked something", Toast.LENGTH_SHORT).show();
+                try {
+                    connectdv();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // Toast.makeText(getApplicationContext(), "You clicked something", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -76,6 +92,113 @@ public class Connect extends AppCompatActivity {
 
 
 
+    public void connectdv() throws IOException, InterruptedException {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+        Iterator<BluetoothDevice> iterator = pairedDevices.iterator();
+        BluetoothDevice device = iterator.next();
+        BluetoothSocket socket = null;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(uuid);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        InputStream istream = null;
+        try {
+            istream = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        OutputStream ostream = null;
+        try {
+            ostream = socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // send
+        ostream.write("whatever\n".getBytes());
+
+
+        // receive
+        Thread.sleep(1000);
+        int avail = istream.available();
+        Log.w(this.getClass().getSimpleName(), "Received " + avail + " bytes");
+        BufferedReader br = new BufferedReader(new InputStreamReader(istream));
+        String s = null;
+        try {
+            s = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.w(this.getClass().getSimpleName(), "Received: " + s);
+
     }
 
+
+    }
+
+
+
+
+
+    /*
+
+    private BluetoothServerSocket mmServerSocket;
+
+    public void ServerBluetooth()
+    {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothServerSocket tmp = null;
+        try
+        {
+            UUID uuid=UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+            tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("Usluga witajaca", uuid);
+        }
+        catch (IOException e)
+        {
+
+        }
+        mmServerSocket = tmp;
+    }
+
+
+
+    public void run_bt()
+    {
+        Log.d("INFO","Uruchamiam serwer");
+        BluetoothSocket socket = null;
+        while (true)
+        {
+            try
+            {
+                Log.d("INFO","Czekam na polaczczenie od clienta");
+                socket = mmServerSocket.accept();
+                Log.d("INFO","Mam clienta!");
+            } catch (IOException e)
+            {
+
+                break;
+            }
+            if (socket != null)
+            {
+
+                try
+                {
+                    mmServerSocket.close();
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+
+*/
 
